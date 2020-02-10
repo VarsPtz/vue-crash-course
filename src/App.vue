25ba41c6ae02e27c5a43f8ca9c150d76ed0b9b78
Alexandr Varygin <otz-job@mail.ru>
2020-02-10 08:14:16 +0300

    add routes

--- src/App.vue
+++ src/App.vue
@@ -4,61 +4,20 @@
     <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
 
     <h1>Todo application</h1>
-    <AddTodo
-      @add-todo="addTodo"
-    />
     <hr>
-    <TodoList
-      v-bind:todos="todos"
-      @remove-todo="removeTodo"
-    />
+    <!--<AddTodo-->
+      <!--@add-todo="addTodo"-->
+    <!--/>-->
+    <!--<hr>-->
+    <!--<TodoList-->
+      <!--v-bind:todos="todos"-->
+      <!--@remove-todo="removeTodo"-->
+    <!--/>-->
+    <router-view />
   </div>
 </template>
 
-<script>
-  // @ всегда указывает в папку /src
-  // импортируем компонент
-import TodoList from '@/components/TodoList'
-import AddTodo from '@/components/AddTodo'
-// import HelloWorld from './components/HelloWorld.vue'
 
-
-  // регистрируем компонент TodoList
-  // TodoList: TodoList   если ключ и значение совпадают, то ключ можно не указывать => TodoList
-export default {
-  name: 'app',
-  data() {
-    return {
-      todos: [
-        // {id: 1, title: 'Купить хлеб', completed: false},
-        // {id: 2, title: 'Купить масло', completed: false},
-        // {id: 3, title: 'Купить пиво', completed: false}
-      ]
-    }
-  },
-  mounted() {
-      fetch('https://jsonplaceholder.typicode.com/todos?_limit=3')
-          .then(response => response.json())
-          // .then(json => console.log(json))
-          .then(json => {
-              this.todos = json
-          })
-  },
-  methods: {
-    removeTodo(id) {
-        this.todos = this.todos.filter(t => t.id !==id)
-    },
-    addTodo(todo) {
-        this.todos.push(todo)
-    }
-  },
-  components: {
-    // HelloWorld
-    TodoList,
-    AddTodo
-  }
-}
-</script>
 
 <style>
 #app {
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>Home page</h2>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, nostrum.</p>
+
+    <a href="/todos">Todos</a>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+
+</style>
